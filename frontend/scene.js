2572b67aeeb1d81081785009a6c4c22036a216b6
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2021-03-29 12:11:10 +0100

    Added board size parameter to genBoard function

--- frontend/scene.js
+++ frontend/scene.js
@@ -1,53 +1,66 @@
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0x87CEEB );
+scene.background = new THREE.Color(0x87CEEB);
 
-const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 1000 );
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const darkwood = new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('images/darkwood.png') } );
-const lightwood = new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('images/lightwood.png') } );
+const textureDarkWood = new THREE.TextureLoader().load('images/darkwood.png');
+const textureLightWood = new THREE.TextureLoader().load('images/lightwood.png');
+const textureDarkMarble = new THREE.TextureLoader().load('images/blackmarble.jpg');
+const textureLightMarble = new THREE.TextureLoader().load('images/whitemarble.png');
 
+const darkwood = new THREE.MeshBasicMaterial({ map: textureDarkWood });
+const lightwood = new THREE.MeshBasicMaterial({ map: textureLightWood });
 
-const darkmarble = new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('images/blackmarble.jpg') } );
-const lightmarble = new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('images/whitemarble.png') } );
+const darkmarble = new THREE.MeshBasicMaterial({ map: textureDarkMarble });
+const lightmarble = new THREE.MeshBasicMaterial({ map: textureLightMarble });
 
 
 const geometry = new THREE.BoxGeometry(0.8, 0.2, 0.8);
-const cube = new THREE.Mesh( geometry, darkwood );
+const cube = new THREE.Mesh(geometry, darkwood);
 // scene.add( cube );
 
 
-camera.position.x =  0;
-camera.position.y = 10;
-camera.position.z =  0;
+camera.position.x = 5;
+camera.position.y = 5;
+camera.position.z = 0;
 camera.lookAt(0, 0, 0);
 
-const animate = function () {
-	requestAnimationFrame( animate );
+function animate() {
+	requestAnimationFrame(animate);
 
 	// cube.rotation.x += 0.01;
 	// cube.rotation.y += 0.01;
-    // cube.position.x += 0.01;
+	// cube.position.x += 0.01;
 
-	renderer.render( scene, camera );
+	renderer.render(scene, camera);
 };
 
 animate();
 
-function genBoard() {
-	const darkBase = new THREE.Mesh( new THREE.BoxGeometry(1, 0.2, 1), darkwood );
-	const lightBase = new THREE.Mesh( new THREE.BoxGeometry(1, 0.2, 1), lightwood );
+function genBoard(size = 5) {
+	const darkBase = new THREE.Mesh(new THREE.BoxGeometry(1, 0.2, 1), darkwood);
+	const lightBase = new THREE.Mesh(new THREE.BoxGeometry(1, 0.2, 1), lightwood);
 
-	for (let z = -2; z <= 2; z++) {
-		for (let x = -2; x <= 2; x++) {
-			const base = (z*5 + x) % 2 ? darkBase.clone() : lightBase.clone();
-			base.position.x = x;
+	let currentBase = lightBase;
+
+	for (let z = 0; z < size; z++) {
+		if (size % 2 === 0) {
+			currentBase = currentBase === lightBase ? darkBase : lightBase;
+		}
+		
+		for (let x = 0; x < size; x++) {
+			const base = currentBase.clone();
+			base.position.x = x - size / 2 + 0.5;
 			base.position.y = 0;
-			base.position.z = z;
-			scene.add( base );
+			base.position.z = z - size / 2 + 0.5;
+
+			currentBase = currentBase === lightBase ? darkBase : lightBase;
+
+			scene.add(base);
 		}
 	}
 }
@@ -55,14 +68,14 @@ function genBoard() {
 //wf, ww, wc, bf, bw, bc
 function genPieceStack(x, z, stack) {
 	let pieces = new Map();
-	pieces.set('bf', new THREE.Mesh( new THREE.BoxGeometry(0.8, 0.2, 0.8), darkmarble  ));
-	pieces.set('wf', new THREE.Mesh( new THREE.BoxGeometry(0.8, 0.2, 0.8), lightmarble ));
+	pieces.set('bf', new THREE.Mesh(new THREE.BoxGeometry(0.8, 0.2, 0.8), darkmarble));
+	pieces.set('wf', new THREE.Mesh(new THREE.BoxGeometry(0.8, 0.2, 0.8), lightmarble));
 
-	pieces.set('bw', new THREE.Mesh( new THREE.BoxGeometry(0.8, 0.8, 0.2), darkmarble  ));
-	pieces.set('ww', new THREE.Mesh( new THREE.BoxGeometry(0.8, 0.8, 0.2), lightmarble ));
+	pieces.set('bw', new THREE.Mesh(new THREE.BoxGeometry(0.8, 0.8, 0.2), darkmarble));
+	pieces.set('ww', new THREE.Mesh(new THREE.BoxGeometry(0.8, 0.8, 0.2), lightmarble));
 	
-	pieces.set('bc', new THREE.Mesh( new THREE.BoxGeometry(0.2, 0.8, 0.2), darkmarble  ));
-	pieces.set('wc', new THREE.Mesh( new THREE.BoxGeometry(0.2, 0.8, 0.2), lightmarble ));	
+	pieces.set('bc', new THREE.Mesh(new THREE.CylinderGeometry(0.25, 0.25, 0.8, 16), darkmarble));
+	pieces.set('wc', new THREE.Mesh(new THREE.CylinderGeometry(0.25, 0.25, 0.8, 16), lightmarble));	
 
 	for (let i = 0; i < stack.length; i++) {
 		let piece = stack[i];
