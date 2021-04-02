161d6140c435d54f84b3d0a7f6b6811a32b70d74
Clara Martins <up201806528@fe.up.pt>
2021-04-02 14:59:22 +0100

    Statistics Documentation and Some Changes to Help with CSV Organization

--- statistics.py
+++ statistics.py
@@ -3,17 +3,21 @@ from random import seed, randint
 import csv
 
 def test_negamax(state, depth, cuts, caching):
+    '''Obtains the total time needed by negamax algorithm to find a solution.'''
     state.negamax(depth, cuts, caching, True)
     return State.total_time
 
-def write_csv(details, times):
+def write_csv(times):
+    '''Write the statistics csv file.'''
     file = open("Statistics.csv", "a")
     csv_file = csv.writer(file)
-    csv_file.writerow(details)
     csv_file.writerow(times)
     file.close()
 
 def test(board_size, depth, cuts, caching):
+    '''Obtains the total time for each of 20 iterations of the negamax algorithm (or until the game ends).'''
+    iterations = 20
+
     details = str(board_size)
     if cuts:
         details += "T"
@@ -25,9 +29,9 @@ def test(board_size, depth, cuts, caching):
         details += "F"
     details += str(depth)
 
-    times = []
+    times = [details]
     state = State(board_size)
-    for _ in range(20):
+    for _ in range(iterations):
         times.append(test_negamax(state, depth, cuts, caching))
         moves = state.possible_moves()
         if len(moves) == 0:
@@ -35,18 +39,19 @@ def test(board_size, depth, cuts, caching):
         move = moves[randint(0, len(moves) - 1)]
         state = move.play(state)
     
-    write_csv(details, times)
+    write_csv(times)
 
 def statistics():
+    '''Obtain statistics for our negamax algorithm.'''
     seed()
     for board_size in range(3, 5):
         for n in range(6):
             test(board_size, n,  True,  True)
         for n in range(5):
             test(board_size, n,  True, False)
-        for n in range(5):
+        for n in range(4):
             test(board_size, n, False,  True)
-        for n in range(5):
+        for n in range(2):
             test(board_size, n, False, False)
     return
 
